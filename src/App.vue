238b531f21ad310a7416e851c5cbf83e0fe75867
aranpando <gbye45@hotmail.com>
2020-03-21 21:06:08 +1100

    Added Vuetify, made values, connected values to table, made the user input filter table, made a dynamic min max

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,129 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app>
+    <AppBar />
+
+    <v-content>
+      <h1 class="text-center mt-5 mb-5">Fnacy Table Filter</h1>
+
+      <v-container>
+        <v-row>
+            <!-- Input -->
+          <v-col cols="12" sm="6" class="mx-auto">
+            <v-text-field
+              v-model="minVal"
+              label="Min value"
+              outlined
+              clearable
+              type='number'
+            ></v-text-field>
+          </v-col>
+
+          <!-- slider -->
+          <v-col cols="12" class="mx-auto">
+            <v-slider
+              v-model="minVal"
+              thumb-label
+              :max="MaxValue"
+              :min="MinValue"
+            />
+          </v-col>
+
+            <!-- table -->
+          <v-col cols="12">
+            <ul>
+              <v-data-table 
+                :headers="headers" 
+                :items="GetValues" 
+                item-key="name" 
+                class="elevation-2"
+                outlined
+              />
+            </ul>
+          </v-col>
+
+        </v-row>
+      </v-container>
+    </v-content>
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import AppBar from './components/AppBar'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+  components: {
+    AppBar,
+  },
+
+  computed: {
+    GetValues() {
+      return this.values.filter(row => row.value >= this.minVal)
+    },
+    MaxValue() {
+      return Math.max.apply(Math, this.values.map(function(row) { return row.value; }))
+    },
+    MinValue() {
+      return Math.min.apply(Math, this.values.map(function(row) { return row.value; }))
+    }
+  },
+
+  data () {
+    return {
+      minVal: null,
+      headers: [
+        {
+          text: 'Email',
+          align: 'start',
+          sortable: false,
+          value: 'email',
+        },
+        { text: 'Value', value: 'value' },
+      ],
+      values: [
+        {
+          email: "example1@gmail.com",
+          value: 1000,
+        },
+        {
+          email: "example2@gmail.com",
+          value: 2000,
+        },
+        {
+          email: "example3@gmail.com",
+          value: 3000,
+        },
+        {
+          email: "example4@gmail.com",
+          value: 4000,
+        },
+        {
+          email: "example5@gmail.com",
+          value: 5000,
+        },
+        {
+          email: "example6@gmail.com",
+          value: 6000,
+        },
+        {
+          email: "example7@gmail.com",
+          value: 7000,
+        },
+        {
+          email: "example8@gmail.com",
+          value: 8000,
+        },
+        {
+          email: "example9@gmail.com",
+          value: 9000,
+        },
+        {
+          email: "example10@gmail.com",
+          value: 10000,
+        },
+      ]
+    }
+  },
+};
+</script>
